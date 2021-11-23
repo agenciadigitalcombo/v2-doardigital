1501d5c932be392a8426eceedac4c2ff8c4f5007
EllSilva <joaquimmigueldecristo@gmail.com>
2021-11-23 16:57:07 -0300

    primeiro

---
+++ assets/js/views/login.js
@@ -0,0 +1,13 @@
+export default {
+    template: `
+    <div">
+   
+    <h1> Olaaooooooo <h1>
+    </div>
+    `,
+    data: function () {
+        return {
+        }
+    },
+   
+}
--- cadastro/assets/js/main.js
+++ cadastro/assets/js/main.js
@@ -3,6 +3,8 @@ import Router from './vendor/vue-router.js'
 import store from './domain/store.js'
 import {mapState} from './vendor/vuex.js'
 
+
+
 Vue.use(Router)
 import routes from './domain/list-router.js'
 const router = new Router({ routes })
