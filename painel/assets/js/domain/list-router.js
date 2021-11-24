a70cc051d4aa398ced237a5dffa5e1ad74ec9fb1
Digital Combo <contato@digitalcombo.com.br>
2021-11-24 15:02:30 -0300

    fix

--- painel/assets/js/domain/list-router.js
+++ painel/assets/js/domain/list-router.js
@@ -1,8 +1,4 @@
 export default [
-<<<<<<< HEAD
-    { path: '/', component: { template: '<h1>Oláaa</h1>' } },    
-=======
     { path: '/', component: { template: '<c-login/>' } },  
     { path: '/dash', component: { template: '<h1>Dash</h1>' } },   
->>>>>>> cc0a09adb009752a8fb4a9614b73ca235e07d541
 ]
