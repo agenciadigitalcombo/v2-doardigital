564e557dc7d11a694d59b56957586dc190a37c59
Victorfer18 <victorfernandomagalhaes@gmail.com>
2021-12-10 10:59:23 -0300

    fix

--- api/controle/CredencialControler.php
+++ api/controle/CredencialControler.php
@@ -36,20 +36,39 @@ class CredencialControler
     }
     static function list_credencial()
     {
+
+        $credencial = new Credencial();
+        $lista_todos = $credencial->list_all();
+        foreach($lista_todos as $g){
+            $payload [] = [
+                'nome' =>  $g['nome_identificacao'],
+                'recursos' => $g['recursos']
+            ];
+        }
         echo json_encode([
             'next' => true,
             'message' => 'Credenciais',
-            'dados' => [
-                ["id" => 1, "nome_identificacao" => "Atendente", "recursos" => "doadores, doações, metas, emails, qrcode"],
-                ["id" => 2, "nome_identificacao" => "Atendente 01", "recursos" => "início, doadores, doações, metas, emails, qrcode, configuração"],
-                ["id" => 3, "nome_identificacao" => "Atendente 02", "recursos" => "Usuários, início, doadores, doações, metas, emails, qrcode, configuração"],
-                ["id" => 4, "nome_identificacao" => "Atendente 03", "recursos" => "início, doadores, doações, metas"],
-            ]
+            'dados' => $payload
         ]);
     }
     static function credencial()
     {
         $credencial = new Credencial();
+        $id = $_REQUEST['id'] ?? '';
+        
+        $get_credencial = $credencial->list_by_id($id);
+        $payload = [
+            'nome_identificacao' => $get_credencial['nome_identificacao'],
+            'recursos' => $get_credencial['recursos']
+        ];
+
+        echo json_encode([
+            'next' => true,
+            'message' => 'Credenciais',
+            'dados' => $payload
+        ]);
+        
+
     }
     static function detete_credencial()
     {
