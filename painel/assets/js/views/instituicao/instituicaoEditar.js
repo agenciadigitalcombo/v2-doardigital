95a99a19bf79591f090910db3c39a3b6134b74a1
EllSilva <joaquimmigueldecristo@gmail.com>
2022-01-12 08:45:58 -0300

    atualizacao54

--- painel/assets/js/views/instituicao/instituicaoEditar.js
+++ painel/assets/js/views/instituicao/instituicaoEditar.js
@@ -23,7 +23,7 @@ export default {
 								
 										<li class="nav-item mt-2">
 											<a class="nav-link text-active-primary ms-0 me-10 py-5 active"
-												href="#/add-instituicoes">Informação da Instituição</a>
+												href="#/add-instituicoes">Editar Informação da Instituição</a>
 										</li> 
 										<li class="nav-item mt-2">
 											<a class="nav-link text-active-primary ms-0 me-10 py-5 "
