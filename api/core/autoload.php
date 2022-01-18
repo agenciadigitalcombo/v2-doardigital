1225a98a8665661114aab15b778c3e1aaea1696f
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-01-18 13:42:19 -0300

    fix

--- api/core/autoload.php
+++ api/core/autoload.php
@@ -4,14 +4,14 @@ $lista = glob(__DIR__ . "/../interfaces/*.php*");
 foreach ($lista as $path) {
     require $path;
 }
+var_dump($lista);
+die;
 
 $lista = glob(__DIR__ . "/../models/*.php*");
 foreach ($lista as $path) {
     require $path;
 }
 
-var_dump($lista);
-die;
 
 $lista = glob(__DIR__ . "/../controle/*.php*");
 foreach ($lista as $path) {
