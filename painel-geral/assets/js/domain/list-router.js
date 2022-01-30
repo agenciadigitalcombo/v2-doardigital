e022f56b64167bfbe54d7338cb14b5f33a27b14f
Digital Combo <contato@digitalcombo.com.br>
2022-01-30 06:39:25 -0300

    fix

--- painel-geral/assets/js/domain/list-router.js
+++
@@ -1,6 +0,0 @@
-export default [    
-    { path: '/', component: { template: '<c-checkout>' } },
-     
-    { path: '/finalizar', component: { template: '<c-finalizar>' } },
-    
-]
--- painel-geral/assets/js/domain/state.js
+++
@@ -1,9 +0,0 @@
-export default {
-    user: {
-        nome: null,
-        email: null,
-        gravatar: null,
-    },
-    token: null,
-    open_menu: false,
-}
