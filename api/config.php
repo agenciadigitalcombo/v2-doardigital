1f9ce9a6c927d5ca3658e42884b22357d2e1a3b1
Digital Combo <contato@digitalcombo.com.br>
2022-02-01 09:29:43 -0300

    fix

--- api/config.php
+++ api/config.php
@@ -3,7 +3,7 @@
 return [
     "host" => "193.46.199.196",
     "banco" => "doar_testedoar",
-    "user" => "	doar_digitalcombo",
+    "user" => "doar_digitalcombo",
     "senha" => "^2hqx&rS",
     "api_key" => "ak_test_zcP5r3XuiDCVdnbcnH5wB2vMOqsPFl",
     "postback_url" => "https://teste.com",
