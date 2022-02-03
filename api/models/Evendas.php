c8e21167d0d0b0c84705331fa2a3c460110fb1c1
brunocriacoes <br.rafael@outlook.com>
2022-02-03 11:14:06 -0300

    fix

---
+++ api/models/Evendas.php
@@ -0,0 +1,70 @@
+<?php
+class Evendas
+{
+
+    static function curl($payload, $token)
+    {
+        $defaults = [
+            CURLOPT_POST           => true,
+            CURLOPT_HEADER         => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL            => "https://api.e-vendas.net.br/api/pedidos",
+            CURLOPT_POSTFIELDS     => json_encode($payload),
+            CURLOPT_HTTPHEADER     => [
+                'Content-Type:application/json',
+                'token:' . $token
+            ]
+        ];
+        $con = curl_init();
+        curl_setopt_array($con, $defaults);
+        $ex = curl_exec($con);
+        curl_close($con);
+        return $ex;
+    }
+    static function send(
+        string $nome,
+        string $email,
+        string $telefone,
+        string $ddd,
+        string $valor,
+        string $status_payment,
+        string $type_payment,
+        string $boleto_url,
+        string $url_pix,
+        string $code_boleto,
+        string $logradouro,
+        string $token,
+        $external_id = 1
+    ) {
+        $payload = [
+            "NUMERO" => $external_id,
+            "NUMEROID" => $external_id,
+            "TRANSACAO" => intval( (time() / 50) + rand(1, 99) ),
+            "COMPRADOREMAIL" => $email,
+            "COMPRADORNOME" => $nome,
+            "COMPRADORDDD" => $ddd,
+            "COMPRADORTELEFONE" => $telefone,
+            "COMPRADORENDERECO" => $logradouro,
+            "STATUSPEDIDO" => $status_payment,
+            "TIPOPAGAMENTO" => $type_payment,
+            "DATAPEDIDO" => date('Y-m-d'),
+            "BOLETOCODIGOBARRA" => $code_boleto,
+            "VALORPEDIDO" => $valor,
+            "URLCHECKOUT" => "",
+            "CODIGORASTREIO" => "",
+            "URLRASTREIO" => "",
+            "BoletoUrl" => $boleto_url,
+            "URLPIX" => $url_pix,
+            "IDPLATAFORMA" => 17,
+            "PedidosProdutos" => [
+                [
+                    "Produtos" => [
+                        "CODIGO" => time(),
+                        "DESCRICAO" => "Doação R$" . $valor
+                    ]
+                ]
+            ]
+        ];
+        return self::curl($payload, $token);
+    }
+}
---
+++ api/models/SendGrid.php
@@ -0,0 +1,67 @@
+<?php
+class Email
+{
+
+    static function header(
+        $to_name,
+        $to_email,
+        $from_name,
+        $from_email
+    ): string {
+        $headers = "MIME-Version: 1.0 \r\n";
+        $headers .= "Content-type: text/html; charset=utf-8 \r\n";
+        $headers .= "To: {$to_name} <{$to_email}> \r\n";
+        $headers .= "From: {$from_name} <{$from_email}> \r\n";
+        return $headers;
+    }
+
+    function send(
+        $to_name,
+        $to_email,
+        $from_name,
+        $from_email,
+        $assunto,
+        $title,
+        $text,
+        $institution_color = '#C00',
+        $institution_name = 'Instituição',
+        $institution_logo = 'default.png',
+        $modelo_html,
+        $print = false
+    ) {
+
+        $headers = self::header(
+            $to_name,
+            $to_email,
+            $from_name,
+            $from_email
+        );
+
+        $message = self::template( [
+            'institution_logo' => $institution_logo,
+            'institution_color' => $institution_color,
+            'institution_nome' => $institution_name,
+            'categoria' => $title,
+            'text' => $text,
+            'from_email' => $from_email,
+            'from_nome' => $from_name
+        ], $modelo_html);
+
+        @mail($to_email, $assunto, $message, $headers);
+
+        if ($print) {
+            echo $message;
+        }
+    }
+
+    static function template(array $payload, string $modelo_html)
+    {
+        $html = file_get_contents(__DIR__ . "/../template_email/{$modelo_html}.html");
+        $html = str_repeat('@@text@@', $payload['text'], $html);
+        foreach ($payload as $k) {
+            $html = str_replace("@@" . $k . "@@", $payload[$k], $html);
+        }
+        $html = trim( str_replace("%20", ' ', $html) );
+        return $html;
+    }
+}
