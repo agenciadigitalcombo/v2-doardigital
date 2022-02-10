2e088511b77b5fbced37eedeeeeec060195c442d
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-02-10 14:13:02 -0300

    fix

--- api/webhook-doacao.php
+++ api/webhook-doacao.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
 header("Access-Control-Allow-Headers: Authorization, Content-Type");
 header("Access-Control-Allow-Origin: *");
 // header('content-type: application/json; charset=utf-8');
 date_default_timezone_set('America/Sao_Paulo');
 
-if(!empty($_REQUEST['debug'])){
-    
+if(!empty($_REQUEST['debug'])){    
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 }
 
-include __DIR__ . "/core/Banco.php";
-include __DIR__ . "/core/help.php";
-include __DIR__ . "/core/Jwt.php";
-include __DIR__ . "/core/autoload.php";
+$payload = $_REQUEST;
+
+$payload = json_encode($payload);
+
+@mail("br.rafael@outlook.com", "webhook" . date("d/m/Y H:i"), $payload);
+@mail("victorfernandomagalhaes@gmail.com", "webhook" . date("d/m/Y H:i"), $payload);
