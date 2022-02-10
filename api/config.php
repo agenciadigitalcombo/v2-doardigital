5a6c06e2dfacc8dcde0920278abce885a97943c6
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-02-10 14:28:33 -0300

    fix

--- api/config.php
+++ api/config.php
@@ -7,5 +7,9 @@ return [
     "senha" => "^2hqx&rS",
     "api_key" => "ak_test_zcP5r3XuiDCVdnbcnH5wB2vMOqsPFl",
     "postback_url" => "https://teste.com",
-    "base" => "https://doardigital.tk"
+    "base" => "https://doardigital.tk",
+    "email_host" => "mail.doardigital.com.br",
+    "email_port" => 465,
+    "email_user" => "contato@doardigital.com.br",
+    "email_pass" => "Prosperidade@121"
 ];
