1e7ea4432026df5a54e46d406095990b47d9305b
EllSilva <joaquimmigueldecristo@gmail.com>
2022-03-07 16:13:31 -0700

    atualizando 223

--- painel-geral/assets/js/components/c-aside.js
+++ painel-geral/assets/js/components/c-aside.js
@@ -1,10 +1,18 @@
 import get_template from './get_template.js'
+import menus from '../vendor/lista-menu.js'
 
 export default {
     data: function () {
         return {
-            title: "aside"
+            title: "aside",
+            menus,
+            lista: [],
         }
     },
+
+    async mounted() {
+        this.lista = this.menus
+    },
+
     template: await get_template('./assets/js/components/c-aside')
 }
