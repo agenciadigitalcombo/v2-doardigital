780a56eb30f02a6e7880eae351ce6556f07ebde4
brunocriacoes <br.rafael@outlook.com>
2022-03-14 09:55:01 -0300

    fix

---
+++ api/models/Asaas.php
@@ -0,0 +1,6 @@
+<?php
+
+class Asaas {
+    const PATH = 'https://www.asaas.com';
+    const PATH_SANDBOX = 'https://sandbox.asaas.com';
+}
---
+++ api/webhook.php
@@ -0,0 +1,6 @@
+<?php
+
+echo json_encode([
+    "next" => true,
+    "message" => "Seja bem vindo a Doar Digital"
+]);
