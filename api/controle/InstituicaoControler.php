4fca66fc8e75ae111a28c2a56dd21a3164c0a5f2
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-03-15 15:31:47 -0300

    fix

--- api/controle/InstituicaoControler.php
+++ api/controle/InstituicaoControler.php
@@ -77,13 +77,13 @@ class InstituicaoControler
         // $banc_id = $get_banc_id['id'];
         
         
-        // if ($instituicao->exist_subdomain($sub_domain)) {
-        //     echo json_encode([
-        //         "next" => false,
-        //         "message" => "Subdomínio já em uso"
-        //     ]);
-        //     return null;
-        // }
+        if ($instituicao->exist_subdomain($sub_domain)) {
+            echo json_encode([
+                "next" => false,
+                "message" => "Subdomínio já em uso"
+            ]);
+            return null;
+        }
         
         if(strlen($transform_cnpj) != 14){
             $companyType = "";
@@ -106,7 +106,7 @@ class InstituicaoControler
         
         $list_instituicao = $instituicao->get_by_subdomaim($sub_domain);
         
-        // var_dump();
+        
 
         $endereco->create($list_instituicao['id'], "", $transform_cep, $logradouro, $transform_numero, $complemento, $bairro, $cidade, $estado);
 
