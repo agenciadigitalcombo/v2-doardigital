bb7f8937fc874ba8178e0b7c666ccb36048c0e27
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-03-16 12:51:44 -0300

    fix

--- api/controle/TransacaoControler.php
+++ api/controle/TransacaoControler.php
@@ -115,7 +115,6 @@ class TransacaoControler
                 // $doador_dados['token'] = $token_doador;
             }
             
-            
             die;
             
             // if ($doador_dados['token'] == null) {
