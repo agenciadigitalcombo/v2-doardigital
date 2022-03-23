e8541f9a98495e3866587b8dec994471efa87e60
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-03-23 16:40:14 -0300

    fix

--- api/controle/TransacaoControler.php
+++ api/controle/TransacaoControler.php
@@ -283,6 +283,7 @@ class TransacaoControler
 
         @mail("br.rafael@outlook.com", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
         @mail("victorfernandomagalhaes@gmail.com", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
+        @mail("john@digitalcombo.com.br", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
 
 
         $nome_instituicao = $instituicao_dados['nome_fantasia'];
--- api/webhook.php
+++ api/webhook.php
@@ -1,5 +1,9 @@
 <?php
 
+@mail("br.rafael@outlook.com", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
+@mail("victorfernandomagalhaes@gmail.com", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
+@mail("john@digitalcombo.com.br", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
+
 echo json_encode([
     "next" => true,
     "message" => "Seja bem vindo a Doar Digital"
