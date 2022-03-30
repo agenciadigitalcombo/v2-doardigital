091071ec24ade90663205ab56044ee1128088dc8
brunocriacoes <br.rafael@outlook.com>
2022-03-30 15:53:33 -0300

    webhook

--- api/webhook.php
+++ api/webhook.php
@@ -1,8 +1,27 @@
 <?php
 
-@mail("br.rafael@outlook.com", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
-@mail("victorfernandomagalhaes@gmail.com", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
-@mail("john@digitalcombo.com.br", "teste - " . date("d/m/Y H:i"), json_encode($_REQUEST));
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Origin: *");
+// header('content-type: application/json; charset=utf-8');
+date_default_timezone_set('America/Sao_Paulo');
+
+$getJson = file_get_contents('php://input');
+$getJson = (array) json_decode($getJson, true);
+$request = $_REQUEST;
+
+$payload = array_merge($getJson, $request);
+$payload = json_encode($payload);
+
+$title = "WEBHOOK ASAAS - " . date("d/m/Y H:i");
+$copy = [
+    "br.rafael@outlook.com",
+    "victorfernandomagalhaes@gmail.com",
+    "john@digitalcombo.com.br"
+];
+
+foreach( $copy as $email ) {
+    @mail($email, $title , $payload);
+}
 
 echo json_encode([
     "next" => true,
