9c6a685b5c0815f8e79948401710d0455c9fa2f5
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-04-05 11:21:10 -0300

    fix

--- api/controle/DoacaoControler.php
+++ api/controle/DoacaoControler.php
@@ -2,12 +2,36 @@
 
 class DoacaoControler{
 
-//     static function list_doacao(){
+    static  function list_doacao(){
         
-//         $doacao = new Doacao();
-        
-//         $doacao_id
-        
-//     }
+        $doacoes = new Doacao();
+
+        $doacao_id = $_REQUEST['doacao_id'];
+
+        campo_obrigatorios([
+            'doacao_id' => 'Indoforme o ID da Doacao',
+        ]);
+
+        $list = $doacoes->get_by_id($doacao_id);
+
+        $payload = [
+        'doacao_id' => $list['id'],
+        'valor' => $list['valor'],
+        'status_pagamento' => $list['status_pagamento'],
+        'codigo' => $list['codigo'],
+        'url' => $list['url'],
+        'data' => $list['data'],
+        'hora' => $list['hora'],
+        'tipo' => $list['tipo']
+        ];
+
+
+
+        echo json_encode([
+            'next' => true,
+            'message' => 'Doacao Pelo Id',
+            'dados' => $payload
+        ]);
+    }
 
 }
