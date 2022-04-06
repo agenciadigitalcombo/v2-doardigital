4ad70fa0dcad8eb0979006bae6f41d0ed71f24ef
Digital Combo <contato@digitalcombo.com.br>
2022-04-06 02:22:16 -0300

    fix

--- api/controle/DoacaoControler.php
+++ api/controle/DoacaoControler.php
@@ -9,7 +9,7 @@ class DoacaoControler{
         $doacao_id = $_REQUEST['doacao_id'];
 
         campo_obrigatorios([
-            'doacao_id' => 'Indoforme o campo doacao_id',
+            'doacao_id' => 'Informe o campo doacao_id',
         ]);
 
         $list = $doacoes->get_by_id($doacao_id);
