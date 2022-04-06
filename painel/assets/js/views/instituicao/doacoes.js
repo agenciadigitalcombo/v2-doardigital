c5f4b259f4ea62c802553be15e45939946f9dce1
Digital Combo <contato@digitalcombo.com.br>
2022-04-05 23:36:19 -0300

    fix

--- painel/assets/js/views/instituicao/doacoes.js
+++ painel/assets/js/views/instituicao/doacoes.js
@@ -345,7 +345,7 @@ export default {
             var tmp = price + '';
             tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
             if (tmp.length > 6)
-                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1.$2");
+                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
 
             return `R$ ${tmp}`;
         },
