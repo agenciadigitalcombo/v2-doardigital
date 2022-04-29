3b03ef1868c565b8a21b15aed3b3cd5c5c017aab
brunocriacoes <br.rafael@outlook.com>
2022-04-29 10:08:08 -0300

    fix

--- api/models/MailBoss.php
+++ api/models/MailBoss.php
@@ -1,32 +1,63 @@
 <?php
 
-class MailBoss{
-    public function create_update( int $instituicao_id, string $token, string $token_uid) : void
+class MailBoss
+{
+    static function subscribe(string $email, string $token)
+    {
+
+        $payload = [
+            "email" => $email,
+            "list_uid" => intval((time() / 50) + rand(1, 99)),
+            "taginternals" => "doar-digital"
+        ];
+        return self::curl($payload, $token);
+    }
+
+    static function curl(array $payload, string $token)
+    {
+        $full_path = "https://member.mailingboss.com/";
+        $full_path .= "/integration/index.php/lists/subscribers/create/{$token}";
+        $defaults = [
+            CURLOPT_POST           => true,
+            CURLOPT_HEADER         => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL            => $full_path,
+            CURLOPT_POSTFIELDS     => http_build_query($payload),
+
+        ];
+        $con = curl_init();
+        curl_setopt_array($con, $defaults);
+        $ex = curl_exec($con);
+        curl_close($con);
+        return $ex;
+    }
+
+    public function register(int $instituicao_id, string $token, string $token_uid): void
     {
         $banco = new Banco();
-        
+
         $exist_id = "SELECT * FROM mailing_boss WHERE instituicao_id=$instituicao_id";
         $res_exist = $banco->query($exist_id);
-        
+
         $set_smtp = "INSERT INTO mailing_boss";
         $set_smtp .= "(instituicao_id, token, token_uid)";
         $set_smtp .= "VALUES";
         $set_smtp .= "($instituicao_id, '$token', '$token_uid')";
-        
+
         $save_smtp = "UPDATE mailing_boss SET token='$token', token_uid='$token_uid'";
-        
-        if(empty($res_exist[0])) {
+
+        if (empty($res_exist[0])) {
             $banco->exec($set_smtp);
-        }else {
+        } else {
             $banco->exec($save_smtp);
         }
     }
 
-    public function get_by_instituicao_id( int $id ): array
+    public function get(int $fk): array
     {
         $banco = new Banco();
-        
-        $sql = "SELECT * FROM mailing_boss WHERE instituicao_id=$id";
+
+        $sql = "SELECT * FROM mailing_boss WHERE instituicao_fk=$fk";
         $guard = $banco->query($sql);
         return $guard[0] ?? [];
     }
