f50a294c531935d8461107d883b7c821fb1c0c80
brunocriacoes <br.rafael@outlook.com>
2022-06-03 13:41:04 -0300

    fix

--- api/loop.php
+++ api/loop.php
@@ -1,6 +1,5 @@
 <?php
 
-
 header("Access-Control-Allow-Headers: Authorization, Content-Type");
 header("Access-Control-Allow-Origin: *");
 // header('content-type: application/json; charset=utf-8');
@@ -13,20 +12,34 @@ if (!empty($_REQUEST['debug'])) {
 
 include __DIR__ . "/core/Banco.php";
 
-$faturas = new Banco();
-$faturas->table("fatura");
-$faturas->where([ "status_pagamento" => "FUTURE" ]);
-$all = $faturas->select();
+$db = new Banco();
+$db->table("message");
+$all = $db->select();
 
-$hoje = strtotime( date('Y-d-m') );
-$validarData = fn($f) => strtotime($f['data']) <= $hoje;
+$action = $all[0] ?? [];
+$action = [
+    "id" => $action["id"] ?? 0,
+    "hoje" => time(),
+    "data" => ($action["data"] ?? time()) + 0,
+    "tipo" => $action["tipo"] ?? "FAIL",
+    "payload" => json_decode($action['payload'] ?? '[]', true)
+];
 
-$all = array_filter( $all, $validarData);
+if ($action["tipo"] == "EMAIL") {
+    $email = $action["payload"]["email"] ?? "br.rafael@outlook.com";
+    mail( $email, "TESTE CRON",  json_encode($action) );
+}
+
+$db->where([
+    "id" => $action["id"]
+]);
+
+$db->delete();
 
 echo json_encode([
     "next" => true,
     "message" => "Lista de agendamentos",
     "payload" => [
-        "faturas" => $all
+        "faturas" => $action
     ],
 ]);
