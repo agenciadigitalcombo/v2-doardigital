9f6c2d99aca7d8ab0910140fd8968a6497c9a1ce
brunocriacoes <br.rafael@outlook.com>
2022-06-08 11:03:33 -0300

    fix

--- api/webhook.php
+++ api/webhook.php
@@ -11,9 +11,27 @@ if (!empty($_REQUEST['debug'])) {
 }
 
 include __DIR__ . "/core/Banco.php";
+include __DIR__ . "/models/Asaas.php";
+include __DIR__ . "/models/AsaasPay.php";
+
+$env = include __DIR__ . "/config.php";
+
+$pay = new AsaasPay();
 
 $faturas = new Banco();
 $faturas->table("fatura");
+
+$sub = new Banco();
+$sub->table("assinatura");
+
+$message = new Banco();
+$message->table("message");
+
+$inst = new Banco();
+$inst->table("institution");
+
+$doadores = new Banco();
+$doadores->table("doador");
 
 $getJson = file_get_contents('php://input');
 $getJson = (array) json_decode($getJson, true);
@@ -22,7 +40,10 @@ $payload = array_merge($getJson, $request);
 
 $subscription =  $payload['payment']['subscription'] ?? "";
 $reference_key = $payload['payment']['externalReference'] ?? "";
+$dueDate = $payload['payment']['dueDate'] ?? "";
 $status = $payload['payment']['status'] ?? "";
+$tipo = $payload['payment']['billingType'] ?? "";
+$url = $payload['payment']['invoiceUrl'] ?? "";
 $ID = $payload['payment']['id'] ?? "";
 
 $title = "WEBHOOK ASAAS - " . date("d/m/Y H:i");
@@ -37,8 +58,88 @@ foreach ($copy as $email) {
 $faturas->where([
     "fatura_id" => $ID
 ]);
+
+$fatura = $faturas->select()[0];
+
 $faturas->update([
     "status_pagamento" => $status,
+]);
+
+$sub->where([
+    "external_fk" => $reference_key
+]);
+$sub->update([
+    "status_pagamento" => $status,
+]);
+
+$institution_fk = $fatura["instituicao_fk"];
+
+$inst->where([
+    "institution_fk" => $institution_fk,
+]);
+
+$company = $inst->select()[0] ?? [];
+
+$doador_fk = $fatura["doador_fk"] ?? "";
+
+$carteira_fk = $company["carteira_fk"] ?? "";
+
+$pay->set_api_key($carteira_fk);
+
+$code = null;
+
+if ($tipo == "PIX") {
+    $resCode = $Pay->getCodePix($ID);
+    $code = $resCode["payload"] ?? "";
+}
+
+if ($tipo == "BOLETO") {
+    $resCode = $pay->getBarcodeBoleto($ID);
+    $code = $resCode["identificationField"] ?? "";
+}
+
+$faturas->where([
+    "fatura_id" => $ID
+]);
+$faturas->update([
+    "codigo" => $code,
+]);
+
+$doadores->where([
+    "external_fk"=> $doador_fk
+]);
+$doador = $doadores->select()[0] ?? [];
+
+$telefone = $doador["telefone"] ?? "";
+
+$payload = json_encode([
+    "instituicao" => $company,
+    "nome" => $fatura["doador_nome"] ?? "",
+    "email" => $fatura["doador_email"] ?? "",
+    "telefone" => $telefone,
+    "valor" => $fatura["valor"] ?? "",
+    "status_payment" => $status,
+    "type_payment" => $tipo,
+    "url" => $url,
+    "code" => $code,
+    "ddd" => $telefone,
+    "boleto_url" => $url,
+    "url_pix" => $code,
+    "code_boleto" => $code,
+    "logradouro" => $company["nome"] ??  "",
+    "token" => $env['evendas'] ?? "",
+    "external_id" => $reference_key,
+]);
+
+$message->insert([
+    "tipo" => "EMAIL",
+    "data" => strtotime($dueDate),
+    "payload" => $payload,
+]);
+$message->insert([
+    "tipo" => "WHATS",
+    "data" => strtotime($dueDate),
+    "payload" => $payload,
 ]);
 
 echo json_encode([
