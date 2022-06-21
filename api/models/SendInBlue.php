10f35c29f11e6fcd646254305addf46f5610621c
brunocriacoes <br.rafael@outlook.com>
2022-06-21 11:29:44 -0300

    fix

--- api/models/SendInBlue.php
+++ api/models/SendInBlue.php
@@ -11,7 +11,7 @@ class SendInBlue {
 
     function __construct()
     {
-        $this->key = "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";
+        $this->key = "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";
         $this->path = "https://api.sendinblue.com/v3/smtp/email";
         $this->header = [
             "accept: application/json",
