7d516b3856eef2e9783793024b9c2bc03b03ae4d
EllSilva <joaquimmigueldecristo@gmail.com>
2022-06-23 13:08:20 -0700

    atualizando 349

--- painel-geral/assets/js/components/c-header.js
+++ painel-geral/assets/js/components/c-header.js
@@ -35,7 +35,7 @@ export default {
     },
 
     created() {
-        
+      
         this.titulo = [window.location.href.split('#/')[1]] || window.location.href.split('#/')[1]
       
     },
--- teste2/assets/js/components/get_template.js
+++
@@ -1,9 +0,0 @@
-async function get_template( path ) {
-    return await fetch(`${path}.html`).then(function (response) {
-        return response.text()
-    }).then(function (html) {
-        return html;
-    })
-}
-
-export default get_template
