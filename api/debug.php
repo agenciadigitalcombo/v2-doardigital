156aff95ecba992fff3ba9646f26990172f0f9cb
brunocriacoes <br.rafael@outlook.com>
2022-06-28 11:49:38 -0300

    fix

--- api/debug.php
+++ api/debug.php
@@ -9,9 +9,36 @@ $invoices = $donations->select();
 
 $subscribe = new Banco();
 $subscribe->table("assinatura");
+$subs = $subscribe->select();
 
+$subs = array_map(fn ($s) => $s["subscription_fk"], $subs);
 
+$invoices = array_filter($invoices, function ($donation) use ($subs) {
+    return stripos($donation["fatura_id"], "sub_") !== false && !in_array($donation["fatura_id"], $subs);
+});
 
+$invoices = array_values($invoices);
 
+$invoices = array_map(function ($donation) {
+    return [
+        "instituicao_fk" => $donation["instituicao_fk"],
+        "external_fk" => $donation["external_fk"],
+        "doador_fk" => $donation["doador_fk"],
+        "subscription_fk" => $donation["fatura_id"],
+        "tipo_pagamento" => $donation["tipo_pagamento"],
+        "status_pagamento" => $donation["status_pagamento"],
+        "valor" => $donation["valor"],
+    ];
+}, $invoices);
 
-echo json_encode( $invoices );
+$sql = array_map( function($donation) {
+    $keys = implode(",", array_keys($donation) );
+    $values =  implode(",", array_map( fn($k) => "'{$k}'", array_values($donation) )) ;
+    return "INSERT INTO assinatura ({$keys}) VALUES ($values) ";
+}, $invoices );
+
+// echo implode(";", $sql);
+// echo $sql[0];
+$subscribe->exec( implode(";", $sql) );
+
+echo json_encode($invoices);
