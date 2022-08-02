7a3383563d64d092b692204848207095d14a1bdb
brunocriacoes <br.rafael@outlook.com>
2022-08-02 10:01:47 -0300

    fix

--- api/controle/DoadorControle.php
+++ api/controle/DoadorControle.php
@@ -72,7 +72,7 @@ class DoadorControle extends Controle
         $db->where([
             "doador_fk" => $fk
         ]);
-        $db->orderByAsc("data");
+        $db->orderByDesc("data");
         $payload["history"] = array_map(["Fatura", "porter"], $db->select() );
         self::printSuccess(
             "Informação doador",
