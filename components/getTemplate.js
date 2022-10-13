773e02d5b9923c30669e74eac64c084792792e7b
brunocriacoes <br.rafael@outlook.com>
2022-10-13 11:31:32 -0300

    fix

--- components/getTemplate.js
+++ components/getTemplate.js
@@ -1,6 +1,7 @@
 export default async  function (path) {
     let dir = window.location.pathname.replace(/\//gi, '')
     dir = window.location.port == '' ? dir + '/' : ''
+    console.log(`${dir}${path}.html`)
     return await fetch(`${dir}${path}.html`).then(function (response) {
         return response.text()
     }).then(function (html) {
