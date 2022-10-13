ab0f437773b2d27765265801c2c58fd91cc8c7d6
johnhoffmannsantos <johnhoffmannsantos@yahoo.com>
2022-10-13 10:58:55 -0300

    fix

--- components/getTemplate.js
+++ components/getTemplate.js
@@ -2,6 +2,7 @@ export default async  function (path) {
     return await fetch(`${path}.html`).then(function (response) {
         return response.text()
     }).then(function (html) {
+        console.log(html)
         return html;
     })
 }
