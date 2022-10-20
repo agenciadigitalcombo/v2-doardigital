a3106f59e9e4a61bfe6f557de5d65089da9112fa
johnhoffmannsantos <johnhoffmannsantos@yahoo.com>
2022-10-20 08:55:21 -0300

    fix

--- painel-new/dashboard.js
+++ painel-new/dashboard.js
@@ -4,6 +4,7 @@ import BreadCrumb from "../components/BreadCrumb.js"
 import Card  from "../components/Card.js"
 import Loader from "../components/Loader.js"
 import Card2 from "../components/Card2.js"
+import CardGeral from "../components/CardGeral.js"
 
 export default {
     data: function() {
@@ -44,178 +45,30 @@ export default {
         BreadCrumb,
         Card,
         Loader,
-        Card2
+        Card2,
+        CardGeral
     },
     template: `
     <div>
 
     <br>
-    <div class="flex flex-wrap">
-        <Card2 :tax="statusEstorno" value="57" text="Total de Doadores" :value="totalQntEstornado" variation="blue" cor="blue" icon="grupo" size="4" />
-        <Card2 :tax="statusEstorno" value="35" text="Novos Doadores" :value="totalQntEstornado" variation="green" cor="green" icon="heart" size="4" />
-        <Card2 :tax="statusEstorno" value="20" text="Doadores Recorrentes" :value="totalQntEstornado" variation="green" cor="green" icon="heart" size="4" />
-        <Card2 :tax="statusEstorno" value="30" text="Doadores Ùnico" :value="totalQntEstornado" variation="yellow" cor="yellow" icon="heart" size="4" />
-        <Card2 :tax="statusEstorno" value="50" text="Doadores Adimplentes" :value="totalQntEstornado" variation="green" cor="green" icon="adimplente" size="4" />
-        <Card2 :tax="statusEstorno" value="20" text="Doadores Inadimplentes" :value="totalQntEstornado" variation="red" cor="red" icon="inadimplente" size="4" />
-        <Card2 :tax="statusEstorno" value="RS 87.000" text="Doação Média" :value="totalQntEstornado" variation="blue" cor="blue" icon="heart" size="4" />
-        <Card2 :tax="statusEstorno" value="RS 10.000" text="Doações Previstas" :value="totalQntEstornado" variation="blue" cor="blue" icon="heart" size="4" />
-
-        <div class="flex flex-row flex-wrap flex-grow mt-2">
-            
-
-            
-
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Graph Card-->
-                <div class="bg-white border-transparent rounded-lg shadow-xl">
-                    <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                        <h2 class="font-bold uppercase text-gray-600">Graph</h2>
-                    </div>
-                    <div class="p-5">
-                        <canvas id="chartjs-0" class="chartjs" width="undefined" height="undefined"></canvas>
-                        <script>
-                            new Chart(document.getElementById("chartjs-0"), {
-                                "type": "line",
-                            "data": {
-                                "labels": ["January", "February", "March", "April", "May", "June", "July"],
-                            "datasets": [{
-                                "label": "Views",
-                            "data": [65, 59, 80, 81, 56, 55, 40],
-                            "fill": false,
-                            "borderColor": "rgb(75, 192, 192)",
-                            "lineTension": 0.1
-                                        }]
-                                    },
-                            "options": { }
-                                });
-                        </script>
-                    </div>
-                </div>
-                <!--/Graph Card-->
-            </div>
-
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Graph Card-->
-                <div class="bg-white border-transparent rounded-lg shadow-xl">
-                    <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                        <h2 class="font-bold uppercase text-gray-600">Graph</h2>
-                    </div>
-                    <div class="p-5">
-                        <canvas id="chartjs-1" class="chartjs" width="undefined" height="undefined"></canvas>
-                        <script>
-                            new Chart(document.getElementById("chartjs-1"), {
-                                "type": "bar",
-                            "data": {
-                                "labels": ["January", "February", "March", "April", "May", "June", "July"],
-                            "datasets": [{
-                                "label": "Likes",
-                            "data": [65, 59, 80, 81, 56, 55, 40],
-                            "fill": false,
-                            "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)", "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"],
-                            "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)"],
-                            "borderWidth": 1
-                                        }]
-                                    },
-                            "options": {
-                                "scales": {
-                                "yAxes": [{
-                                "ticks": {
-                                "beginAtZero": true
-                                                }
-                                            }]
-                                        }
-                                    }
-                                });
-                        </script>
-                    </div>
-                </div>
-                <!--/Graph Card-->
-            </div>
-
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Graph Card-->
-                <div class="bg-white border-transparent rounded-lg shadow-xl">
-                    <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                        <h5 class="font-bold uppercase text-gray-600">Graph</h5>
-                    </div>
-                    <div class="p-5"><canvas id="chartjs-4" class="chartjs" width="undefined" height="undefined"></canvas>
-                        <script>
-                            new Chart(document.getElementById("chartjs-4"), {
-                                "type": "doughnut",
-                            "data": {
-                                "labels": ["P1", "P2", "P3"],
-                            "datasets": [{
-                                "label": "Issues",
-                            "data": [300, 50, 100],
-                            "backgroundColor": ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
-                                        }]
-                                    }
-                                });
-                        </script>
-                    </div>
-                </div>
-                <!--/Graph Card-->
-            </div>
-
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Table Card-->
-                <div class="bg-white border-transparent rounded-lg shadow-xl">
-                    <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                        <h2 class="font-bold uppercase text-gray-600">Graph</h2>
-                    </div>
-                    <div class="p-5">
-                        <table class="w-full p-5 text-gray-700">
-                            <thead>
-                                <tr>
-                                    <th class="text-left text-blue-900">Name</th>
-                                    <th class="text-left text-blue-900">Side</th>
-                                    <th class="text-left text-blue-900">Role</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                <tr>
-                                    <td>Obi Wan Kenobi</td>
-                                    <td>Light</td>
-                                    <td>Jedi</td>
-                                </tr>
-                                <tr>
-                                    <td>Greedo</td>
-                                    <td>South</td>
-                                    <td>Scumbag</td>
-                                </tr>
-                                <tr>
-                                    <td>Darth Vader</td>
-                                    <td>Dark</td>
-                                    <td>Sith</td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                        <p class="py-2"><a href="#">See More issues...</a></p>
-
-                    </div>
-                </div>
-                <!--/table Card-->
-            </div>
-
-            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                <!--Advert Card-->
-                <div class="bg-white border-transparent rounded-lg shadow-xl">
-                    <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                        <h2 class="font-bold uppercase text-gray-600">Advert</h2>
-                    </div>
-                    <div class="p-5 text-center">
-
-
-
-
-
-                    </div>
-                </div>
-                <!--/Advert Card-->
-            </div>
-
+        <div class="flex flex-wrap">
+            <Card2 :tax="statusEstorno" value="57" text="Total de Doadores" :value="totalQntEstornado" variation="blue" cor="blue" icon="grupo" size="4" />
+            <Card2 :tax="statusEstorno" value="35" text="Novos Doadores" :value="totalQntEstornado" variation="green" cor="green" icon="heart" size="4" />
+            <Card2 :tax="statusEstorno" value="20" text="Doadores Recorrentes" :value="totalQntEstornado" variation="green" cor="green" icon="heart" size="4" />
+            <Card2 :tax="statusEstorno" value="30" text="Doadores Ùnico" :value="totalQntEstornado" variation="yellow" cor="yellow" icon="heart" size="4" />
+            <Card2 :tax="statusEstorno" value="50" text="Doadores Adimplentes" :value="totalQntEstornado" variation="green" cor="green" icon="adimplente" size="4" />
+            <Card2 :tax="statusEstorno" value="20" text="Doadores Inadimplentes" :value="totalQntEstornado" variation="red" cor="red" icon="inadimplente" size="4" />
+            <Card2 :tax="statusEstorno" value="RS 87.000" text="Doação Média" :value="totalQntEstornado" variation="blue" cor="blue" icon="heart" size="4" />
+            <Card2 :tax="statusEstorno" value="RS 10.000" text="Doações Previstas" :value="totalQntEstornado" variation="blue" cor="blue" icon="heart" size="4" />
+            <CardGeral text="Faturamento" size="full"/>
+            <CardGeral text="Quantidade de Doações" size="full"/>
+            <CardGeral text="Formas de Pagamentos" size="quatro"/>
+            <CardGeral text="Quantidade por Planos" size="quatro"/>
+            <CardGeral text="Status das Doações" size="quatro"/>
+            <CardGeral text="Tipos dos Doadores" size="quatro"/>
+            <CardGeral text="Status dos Doadores" size="quatro"/>
+            <CardGeral text="Status das Assinaturas" size="quatro"/>
 
         </div>
     </div>`,
