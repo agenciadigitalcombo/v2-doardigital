779ace50be5c1310075af532fdebeaf153b81e3b
johnhoffmannsantos <johnhoffmannsantos@yahoo.com>
2022-10-20 06:33:19 -0300

    fix

--- painel-new/recorrencia.js
+++ painel-new/recorrencia.js
@@ -6,13 +6,13 @@ import actions from "../components/actions.js"
 import ApiDoacoes from "../components/apiDoacoes.js"
 import MyInstitution from "../components/myInstitution.js"
 import { data, formataMoeda, formatRecorrente } from "../components/format.js"
-import Filtro from "../components/Filtro.js"
 import FiltroNome from "../components/FiltroNome.js"
 
 export default {
     data: function () {
         return {
             donations: [],
+            donationsCopy: [],
             totalQuantidade: 0,
             totalMoney:  0,
             cols: {
@@ -32,7 +32,6 @@ export default {
         Table,
         BreadCrumb,
         Card,
-        Filtro,
         FiltroNome
     },
     async mounted() {
@@ -47,8 +46,8 @@ export default {
                 let data_donation = d.dataCreated
                 return hoje < data_donation
             })
-            all_donations = all_donations.reverse()
             this.donations = all_donations
+            this.donationsCopy = all_donations
             this.totalQuantidade = all_donations.length
             this.totalMoney = this.somaAll(all_donations)
         }
@@ -75,6 +74,80 @@ export default {
                 return acc
             }, 0))
         },
+        filtrar(payload) {
+            let dados = Array.from(this.donationsCopy.map(x => x))
+            if (payload.search) {
+                dados = dados.filter(t => {
+                    let search = payload.search
+                    let termo = t.name + " " + t.email + " " + t.cpf
+                    search = search.toLowerCase()
+                    termo = termo.toLowerCase()
+                    search = search.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                    termo = termo.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+                    return termo.indexOf(search) > -1
+                })
+            }
+            if (payload.tipo.length > 2) {
+                dados = dados.filter(t => {
+                    return t.tipo == payload.tipo
+                })
+            }
+            if (payload.recorrencia.length > 2) {
+                dados = dados.filter(t => {
+                    return t.recorrente == payload.recorrencia
+                })
+            }
+            if (payload.status.length > 2) {
+
+                if (payload.status == 'vencido') {
+                    dados = dados.filter(t => {
+                        return t.status == 'OVERDUE'
+                    })
+                }
+                if (payload.status == 'pendente') {
+                    dados = dados.filter(t => {
+                        return t.status == 'PENDING'
+                    })
+                }
+                if (payload.status == 'pago') {
+                    dados = dados.filter(t => {
+                        return t.status == 'RECEIVED' || t.status == 'CONFIRMED'
+                    })
+                }
+                if (payload.status == 'estornado') {
+                    dados = dados.filter(t => {
+                        return t.status == 'REFUNDED'
+                    })
+                }
+            }
+            if (payload.date) {
+                
+                let hoje = moment().format('YYYY-MM-DD')
+                let dataSelecionada = payload.date
+
+                let presente = hoje == dataSelecionada
+                let ontem = dataSelecionada == moment().subtract(1, 'day').format('YYYY-MM-DD')
+                if (presente) {
+                    dados = dados.filter(t => {
+                        let dataDonation = t.dataCreated
+                        return dataDonation == hoje
+                    })
+                } else if (ontem) {
+                    
+                    dados = dados.filter(t => {
+                        let dataDonation = t.dataCreated
+                        return dataDonation == dataSelecionada
+                    })
+                } else {
+                    dados = dados.filter(t => {
+                        let dataDonation = t.dataCreated
+                        return dataDonation >= dataSelecionada
+                    })
+                }
+            }
+            this.totalFaturas = dados.length
+            this.donations = dados
+        }
     },
     template: `
     <div>
@@ -87,7 +160,7 @@ export default {
                     <Card text="Total em Doações Previstas" :value="totalMoney" variation="blue" icon="bar" size="2" />
                 </div>
                 <br>
-                <FiltroNome />
+                <FiltroNome @filter="filtrar"/>
                 <Table :rows="donations" :cols="cols" pagination="12" />
              </div>
           </div>
