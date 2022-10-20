3bcdd3b67f326ac394e7ba0ed916c27d3fcf2f54
johnhoffmannsantos <johnhoffmannsantos@yahoo.com>
2022-10-20 09:35:20 -0300

    fix

--- painel-new/dashboard.js
+++ painel-new/dashboard.js
@@ -42,73 +42,73 @@ export default {
     },
     mounted(){
         var options = {
-  chart: {
-    width: "100%",
-    height: 380,
-    type: "bar"
-  },
-  plotOptions: {
-    bar: {
-      horizontal: true
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  stroke: {
-    width: 1,
-    colors: ["#fff"]
-  },
-  series: [
-    {
-      data: [44, 55, 41, 64, 22, 43, 21]
-    },
-    {
-      data: [53, 32, 33, 52, 13, 44, 32]
-    }
-  ],
-  xaxis: {
-    categories: [
-      "Korea",
-      "Canada",
-      "Poland",
-      "Italy",
-      "France",
-      "Japan",
-      "China"
-    ]
-  },
-  legend: {
-    position: "right",
-    verticalAlign: "top",
-    containerMargin: {
-      left: 35,
-      right: 60
-    }
-  },
-  responsive: [
-    {
-      breakpoint: 1000,
-      options: {
-        plotOptions: {
-          bar: {
-            horizontal: false
-          }
-        },
-        legend: {
-          position: "bottom"
-        }
-      }
-    }
-  ]
-};
-
-var chart = new ApexCharts(
-  document.querySelector("#responsive-chart"),
-  options
-);
-
-chart.render();
+            chart: {
+              width: "100%",
+              height: 380,
+              type: "bar"
+            },
+            plotOptions: {
+              bar: {
+                horizontal: true
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              width: 1,
+              colors: ["#fff"]
+            },
+            series: [
+              {
+                data: [44, 55, 41, 64, 22, 43, 21]
+              },
+              {
+                data: [53, 32, 33, 52, 13, 44, 32]
+              }
+            ],
+            xaxis: {
+              categories: [
+                "Korea",
+                "Canada",
+                "Poland",
+                "Italy",
+                "France",
+                "Japan",
+                "China"
+              ]
+            },
+            legend: {
+              position: "right",
+              verticalAlign: "top",
+              containerMargin: {
+                left: 35,
+                right: 60
+              }
+            },
+            responsive: [
+              {
+                breakpoint: 1000,
+                options: {
+                  plotOptions: {
+                    bar: {
+                      horizontal: false
+                    }
+                  },
+                  legend: {
+                    position: "bottom"
+                  }
+                }
+              }
+            ]
+          };
+          
+          var chart = new ApexCharts(
+            document.querySelector("#responsive-chart"),
+            options
+          );
+          
+          chart.render();
     },
     components: {
         Table,
@@ -136,7 +136,7 @@ chart.render();
             <CardGeral text="Quantidade de Doações" size="full"/>
             <CardGeral text="Formas de Pagamentos" size="quatro"/>
             <CardGeral text="Quantidade por Planos" size="quatro"> 
-            <div id="chart">
+            <div id="chart" style="max-width: 760px;">
             <div id="responsive-chart"></div>
             </div>
             </CardGeral>
