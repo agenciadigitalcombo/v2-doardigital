26e07b6e3b3ef4817976dd7c5239ea46a0eccd68
brunocriacoes <br.rafael@outlook.com>
2022-11-16 15:36:17 -0300

    fix

--- login/recuperar.js
+++ login/recuperar.js
@@ -6,54 +6,52 @@ import Jwt from "../components/jwt.js"
 
 
 export default {
-    data: function() {
-        return {
-            email: 'johnhoffmannsantos@yahoo.com',
-            onerror: null,
-            statusBtn: true
-        }
-    },
-    methods: {
+   data: function () {
+      return {
+         email: 'johnhoffmannsantos@yahoo.com',
+         onerror: null,
+         statusBtn: true
+      }
+   },
+   methods: {
       redirect(level) {
-          let lb = {
-              sub: 'painel-sub',
-              adm: 'painel-new',
-              super: 'painel-super',
-          }
-          window.location.href = `//${window.location.host}/${lb[level]}`
+         let lb = {
+            sub: 'painel-sub',
+            adm: 'painel-new',
+            super: 'painel-super',
+         }
+         window.location.href = `//${window.location.host}/${lb[level]}`
       },
       async login() {
-          this.onerror = null
-          this.statusBtn = false
-          let api = new apiAdmin()
-          let jwt = new Jwt()
-          let request = await api.recuperar_senha(this.email)
-          this.statusBtn = true
-          if (!request.next) {
-              this.onerror = request.message
-              return null
-          }
-          
+         this.onerror = null
+         this.statusBtn = false
+         let api = new apiAdmin()
+         let jwt = new Jwt()
+         let request = await api.recuperar_senha(this.email)
+         this.statusBtn = true
+         this.onerror = request.message
+         
+
       }
-  },
-  async mounted() {
+   },
+   async mounted() {
       let api = new apiAdmin()
-      let jwt = new Jwt() 
+      let jwt = new Jwt()
       let code = jwt.get()?.code
-      let requestInfo = await api.info(code)      
+      let requestInfo = await api.info(code)
       let level = 'sub'
       if (requestInfo?.payload?.adm?.length == 0) {
-          level = 'adm'
+         level = 'adm'
       }
       if (requestInfo?.payload?.sass == '1') {
-          level = 'super'
+         level = 'super'
       }
       if (jwt.logged()) {
-          this.redirect(level)
+         this.redirect(level)
       }
-  },
+   },
 
-    template: `
+   template: `
     <div>
     <!DOCTYPE html>
    <html lang="pt-br">
@@ -103,7 +101,7 @@ export default {
                                           clip-rule="evenodd"></path>
                                  </svg>
                                  <div>
-                                       <span class="font-medium">Error!</span> {{onerror}}
+                                    {{onerror}}
                                  </div>
                               </div>
                            </div>
