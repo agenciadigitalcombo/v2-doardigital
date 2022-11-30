abc095de8bfe9dbd51789e9dbf0b7050d8ce9905
brunocriacoes <br.rafael@outlook.com>
2022-11-30 15:37:12 -0300

    fix

--- finalizar-assinatura/finalizar.js
+++ finalizar-assinatura/finalizar.js
@@ -4,7 +4,29 @@ const html = await getTemplate( './finalizar' )
 
 export default {
     data: function () {
-        return {}
+        return {
+            plano: 'instituicao',
+            cupom: null,
+            nome: null,
+            numero: null,
+            cvv: null,
+            vencimento: null,
+            valorAssinatura: 98.90,
+            valorAssinaturaPrint: '98,90',
+            planos: [
+                { id: 'instituicao', name: 'Instituição' ,value: 98.90, printValue: '98,90', disparos: 1000 },
+                { id: 'igreja', name: 'Paróquia / Igreja' ,value: 98.90, printValue: '98,90', disparos: 1000 },
+                { id: 'oracao', name: 'Grupo de Oração' ,value: 49.90, printValue: '49,90', disparos: 500 },
+                { id: 'missionario', name: 'Missionário' ,value: 49.90, printValue: '49,90', disparos: 500 },
+            ]
+        }
+    },
+    watch: {
+        plano( idPlanos ) {
+            let plano = this.planos.find( p => p.id == idPlanos )
+            this.valorAssinatura = plano.value
+            this.valorAssinaturaPrint = plano.printValue           
+        }
     },
     components: {},
     async mounted() {},
