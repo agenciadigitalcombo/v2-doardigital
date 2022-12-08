6737ef64b9587280c13785f614ffefbc1a1da66a
brunocriacoes <br.rafael@outlook.com>
2022-12-08 14:37:20 -0300

    fix

--- components/getTemplate.js
+++ components/getTemplate.js
@@ -1,11 +1,11 @@
 export default async  function (path) {
     let full_url = path
-    if(window.location.port == '') {        
-        let dir = window.location.pathname.replace(/\//gi, '')
-        path = path.replace(/.\/..\//gi, '')
-        path = path.replace(`${dir}/`, '')
-        full_url = `../${path}`
-    }
+    // if(window.location.port == '') {        
+    //     let dir = window.location.pathname.replace(/\//gi, '')
+    //     path = path.replace(/.\/..\//gi, '')
+    //     path = path.replace(`${dir}/`, '')
+    //     full_url = `../${path}`
+    // }
     console.log(`${full_url}.html`)
     return await fetch(`${full_url}.html`).then(function (response) {
         return response.text()
