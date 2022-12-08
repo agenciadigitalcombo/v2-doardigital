08c6a330a07bec9fa948e90c5e1c88364faa8dc3
brunocriacoes <br.rafael@outlook.com>
2022-12-08 14:32:20 -0300

    fix

--- finalizar-assinatura/finalizar.js
+++ finalizar-assinatura/finalizar.js
@@ -7,7 +7,7 @@ import Tmp from '../components/Temp.js'
 
 
 
-const html = await getTemplate('finalizar')
+const html = await getTemplate('finalizar-assinatura/finalizar')
 
 export default {
     data: function () {
