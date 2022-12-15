44ce5ea33cf9bbcb419c3d0f16423b8002079136
brunocriacoes <br.rafael@outlook.com>
2022-12-15 16:32:51 -0300

    fix

--- api/routers/whats-template.php
+++ api/routers/whats-template.php
@@ -5,4 +5,4 @@ router('/whats-template/save', 'EmailTemplateWhatsapp@save');
 router('/whats-template/info', 'EmailTemplateWhatsapp@info');
 router('/whats-template/recover', 'EmailTemplateWhatsapp@recover');
 router('/whats-template/list', 'EmailTemplateWhatsapp@list');
-router('/whats-template/list', 'EmailTemplateWhatsapp@install');
+router('/whats-template/install', 'EmailTemplateWhatsapp@install');
