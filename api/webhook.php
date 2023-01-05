88f1bdd38b228b9e35b9ed3b8d8654c1ac14387b
brunocriacoes <br.rafael@outlook.com>
2023-01-05 16:26:38 -0300

    fix

--- api/webhook.php
+++ api/webhook.php
@@ -6,6 +6,9 @@ header("Access-Control-Allow-Origin: *");
 header('content-type: application/json; charset=utf-8');
 date_default_timezone_set('America/Sao_Paulo');
 
+header("HTTP/1.1 200 OK");
+http_response_code(404);
+
 if (!empty($_REQUEST['debug'])) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
